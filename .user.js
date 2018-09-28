6d43cd4a0f4d367880560922061d835216ea1fb9
unknown555 <43687875+unknown555@users.noreply.github.com>
2018-09-28 18:10:11 -0500

    Update .user.js

--- .user.js
+++ .user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         AutoTrimps-genBTC
 // @version      2.1.6.9-genbtc-3-23-2018
-// @namespace    https://genbtc.github.io/AutoTrimps
-// @updateURL    https://genbtc.github.io/AutoTrimps/.user.js
+// @namespace    https://unknown555.github.io/AutoTrimps
+// @updateURL    https://unknown555.github.io/AutoTrimps/.user.js
 // @description  Automate all the trimps!
 // @author       zininzinin, spindrjr, Ishkaru, genBTC
 // @include      *trimps.github.io*
 // @include      *kongregate.com/games/GreenSatellite/trimps
-// @connect      *genbtc.github.io/AutoTrimps*
+// @connect      *unknown555.github.io/AutoTrimps*
 // @connect      *trimps.github.io*
 // @connect      self
 // @grant        none
